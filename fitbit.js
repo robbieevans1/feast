226c0fd5cbbc1f8e73c9817d018524a898fd0793
robcabobb <robcabobb@gmail.com>
2022-04-01 23:29:42 -0400

    refactored code to include promise.all statement

--- fitbit.js
+++ fitbit.js
@@ -11,23 +11,43 @@ let today = year + "-" + month + "-" + day;
 const access_token =
 	"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzg5Q0QiLCJzdWIiOiI0M1BHRFYiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd3BybyB3bnV0IHdzbGUgd3dlaSB3c29jIHdzZXQgd2FjdCB3bG9jIiwiZXhwIjoxNjUwNTUwNDE2LCJpYXQiOjE2NDc5NTg0MTZ9.rxaM9ibMMaTpOGQ-k7aYaiIs7CB9J8ydNHuGcIsAA20";
 
-fetch(`https://api.fitbit.com/1/user/-/activities/date/${today}.json`, {
-	method: "GET",
-	headers: { Authorization: "Bearer " + access_token },
-})
-	.then((response) => {
+
+	activityUrl = `https://api.fitbit.com/1/user/-/activities/date/${today}.json`;
+	foodUrl = `https://api.fitbit.com/1/user/-/foods/log/caloriesIn/date/${today}/1d.json`;
+	
+	function jsonFromResponse (response) {
 		console.log(response);
 		if (!response.ok) {
-			throw Error("Error");
+				throw Error("Error");
 		}
 		return response.json();
-	})
-	.then((data) => {
-		console.log(data);
-		
-    // add calories out data to html
-    const caloriesBurned = `<p>Calories Burned: ${data.summary.caloriesOut}</p>`;
-		document.querySelector("#calories-burned").innerHTML = caloriesBurned;
+	}
+	
+	const getWithAuth = {
+		method: "GET",
+		headers: { Authorization: "Bearer " + access_token },
+	};
+	
+	Promise.all([
+		fetch(activityUrl, getWithAuth)
+				.then(jsonFromResponse),
+		fetch(foodUrl, getWithAuth)
+				.then(jsonFromResponse)
+	])
+		.then(([data, foodData]) => {
+				console.log({data, foodData});
+	
+				// add calories out data to html
+				let caloriesBurned = `<p>Calories Burned: ${data.summary.caloriesOut}</p>`;
+				document.querySelector("#calories-burned").innerHTML = caloriesBurned;
+	
+				// add calories out foodData to html
+				let caloriesEaten = `<p>Calories Eaten: ${foodData["foods-log-caloriesIn"][0].value}</p>`;
+				document.querySelector("#calories-eaten").innerHTML = caloriesEaten;
+	
+				let calorieDeficit = "<p>Desired Deficit: 500</p>";
+				document.querySelector("#calorie-deficit").innerHTML = calorieDeficit;
+
 
     // adds bmr data to html
 		const bmr = `<p>BMR: ${data.summary.caloriesBMR}</p>`;
@@ -44,30 +64,16 @@ fetch(`https://api.fitbit.com/1/user/-/activities/date/${today}.json`, {
     // add resting heart rate data to html
 		const restingHeartRate = `<p>Resting Heart Rate: ${data.summary.restingHeartRate}</p>`;
 		document.querySelector("#heart-rate").innerHTML = restingHeartRate;
-	});
-
-  fetch(`https://api.fitbit.com/1/user/-/foods/log/caloriesIn/date/${today}/1d.json`, {
-	method: "GET",
-	headers: { Authorization: "Bearer " + access_token },
-})
-	.then((response) => {
-		console.log(response);
-		if (!response.ok) {
-			throw Error("Error");
-		}
-		return response.json();
-	})
-	.then((data) => {
-		console.log(data);
-		
-    // add calories out data to html
-    const caloriesEaten = `<p>Calories Eaten: ${data["foods-log-caloriesIn"][0].value}</p>`;
-		document.querySelector("#calories-eaten").innerHTML = caloriesEaten;
-	});
-
-  calorieDeficit = "Desired Deficit: 500"
-  document.querySelector("#calorie-deficit").innerHTML = calorieDeficit;
-
- 
-
-
+				
+				calorieDeficit = 500;
+				
+				// redeclaring variable from activity url  
+				caloriesBurned = data.summary.caloriesOut
+				
+				// redeclaring variable from food url  
+				caloriesEaten = foodData["foods-log-caloriesIn"][0].value
+				
+				const caloriesAvailable = `<p> Calories Available<span class="text-warning"> Right Now</span>: ${caloriesBurned - caloriesEaten - calorieDeficit}`
+				document.querySelector("#calories-available").innerHTML = caloriesAvailable;
+		});
+	
